002983af21154fad9919bd17d1b32982ae8597ef
PriyankaChoure <priyanka.choure10@gmail.com>
2026-05-27 12:36:37 +0530

    update product detail page to add more details

--- src/components/ProjectDetals.jsx
+++ src/components/ProjectDetals.jsx
@@ -26,36 +26,45 @@ const ProjectDetails = ({project, isOpen, onClose}) => {
                     <div className="mb-6">
                         <div>
                     <h3 className="text-3xl font-bold text-gray-900 mb-2">Project</h3>
-                    <span className="text-xl font-semibold text-gray-900">Code:</span>
+                    <span className="text-xl font-semibold text-gray-900 mr-10">Code:</span>
                     <span className="text-xl font-semibold text-gray-600">{project.projectCode}</span>
                     <br/>
-                    <span className="text-xl font-semibold text-gray-900">Title:</span>
+                    <span className="text-xl font-semibold text-gray-900 mr-2">Product:</span>
+                    <span className="text-xl font-semibold text-gray-600"> {project.product}</span>
+                    <br/>
+                    <span className="text-xl font-semibold text-gray-900 mr-10">Title:</span>
                     <span className="text-xl font-semibold text-gray-600"> {project.projectTitle}</span>
                     </div>
                     <div>
                         <br/>
-                        <h3 className="text-3xl font-bold text-gray-900 mb-2">Details</h3>
+                        <h3 className="text-3xl font-bold text-gray-900 mb-2 mr-4">Details</h3>
                         <p className="text-xl font-bold text-gray-600">{project.projectDetails}</p>
                     </div>
                     <div>
                         <br/>
-                        <span className="text-xl font-semibold text-gray-900">Status:</span>
+                        <span className="text-xl font-semibold text-gray-900 mr-4">Status:</span>
                         <span className="text-xl font-semibold text-gray-600">{project.status}</span>
                         <br/>
-                        <span className="text-xl font-semibold text-gray-900">State:</span>
+                        <span className="text-xl font-semibold text-gray-900 mr-8">City:</span>
+                        <span className="text-xl font-semibold text-gray-600"> {project.city}</span>
+                        <br/>
+                        <span className="text-xl font-semibold text-gray-900 mr-4">State:</span>
                         <span className="text-xl font-semibold text-gray-600"> {project.state}</span>
                     </div>
                     <div>
                         <br/>
                         <h3 className="text-3xl font-bold text-gray-900 mb-2">Contact Details</h3>
-                        <span className="text-xl font-semibold text-gray-900">Contact:</span>
+                        <span className="text-xl font-semibold text-gray-900 mr-4">Contact:</span>
                         <span className="text-xl font-semibold text-gray-600">{project.contactDetails}</span>
                         <br/>
-                        <span className="text-xl font-semibold text-gray-900">Expected Completion Date:</span>
-                        <span className="text-xl font-semibold text-gray-600">{project?.expectedCompletionDate}</span>
+                        <span className="text-xl font-semibold text-gray-900 mr-2">Expected Completion Date:</span>
+                        <span className="text-xl font-semibold text-gray-600">{project?.expectedCompletionDate ?? "N/A"}</span>
                         <br/>
-                        <span className="text-xl font-semibold text-gray-900">Contractor Name:</span>
+                        <span className="text-xl font-semibold text-gray-900 mr-4">Contractor Name:</span>
                         <span className="text-xl font-semibold text-gray-600">{project.contractor}</span>
+                        <br/>
+                        <span className="text-xl font-semibold text-gray-900 mr-12">Project Cost:</span>
+                        <span className="text-xl font-semibold text-gray-600">{project.projectValue}</span>
                         <br/>                        
                     </div>
                     
